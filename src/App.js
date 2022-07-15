0147735270b123b62011e775f160bd81f1286246
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-07-15 16:07:54 +0200

    append theme context to app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import Layout from './layouts';
+import Homepage from './pages/homepage';
+import { ThemeProvider } from './context/theme';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		// detect theme
+		if (
+			localStorage.getItem('theme') === 'dark' ||
+			(!('theme' in localStorage) &&
+				window.matchMedia('(prefers-color-scheme: dark)').matches)
+		) {
+			document.body.classList.add('dark');
+		} else {
+			document.body.classList.remove('dark');
+		}
+	}, []);
+
+	return (
+		<ThemeProvider>
+			<Layout>
+				<Homepage />
+			</Layout>
+		</ThemeProvider>
+	);
 }
 
 export default App;
